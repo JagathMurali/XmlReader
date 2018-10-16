4a5097c1ef24003a23f81565b958b77bdf279802
Jagath Murali <jagathmurali@gmail.com>
2018-10-17 00:06:13 +1100

    Added test cases and sample files for the same

---
+++ UnitTest/XMLParserUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using CodeTest.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class XMLParserUnitTest
+    {
+        [TestMethod]
+        public void CheckXmlFileIsAddedIntoQueueAfterReadingAndParsing()
+        {
+            // Arrange
+            XMLStringQueue sampleQueue = new XMLStringQueue();
+            XMLParser xmlparser = new XMLParser(sampleQueue);
+            xmlparser.FolderPath = @"..\..\SampleInput\";
+
+            // Act
+            xmlparser.GetFilesFromFolder();
+
+            // Assert
+            Assert.IsFalse(sampleQueue.xmlConverterQueue.IsEmpty);
+            Assert.AreEqual(sampleQueue.xmlConverterQueue.Count, 1);
+        }
+
+        [TestMethod]
+        public void CheckXmlFileIsAddedIntoQueueAfterReadingAndParsing()
+        {
+            // Arrange
+            XMLStringQueue sampleQueue = new XMLStringQueue();
+            XMLParser xmlparser = new XMLParser(sampleQueue);
+            xmlparser.FolderPath = @"..\..\SampleInput\";
+
+            // Act
+            xmlparser.GetFilesFromFolder();
+
+            // Assert
+            Assert.IsFalse(sampleQueue.xmlConverterQueue.IsEmpty);
+            Assert.AreEqual(sampleQueue.xmlConverterQueue.Count, 1);
+        }
+    }
+}
--- XmlHtmlConversion/Models/XMLParser.cs
+++ XmlHtmlConversion/Models/XMLParser.cs
@@ -12,21 +12,20 @@ namespace CodeTest.Models
         {
             stringQueue = _xmlStringQueue;
         }
+        public void GetFilesFromFolder()
+        {
+            foreach (string file in Directory.EnumerateFiles(FolderPath))
+            {
+                string xmlString = XDocumentToStringConverter(file);
+                stringQueue.xmlConverterQueue.Enqueue(xmlString);
+            }
+        }
         private string XDocumentToStringConverter(string file)
         {
 
             XDocument documuent = XDocument.Load(file);
             string xmlString = documuent.ToString();
             return xmlString;
-        }
-       
-        public void GetFilesFromFolder()
-        {
-            foreach (string file in Directory.EnumerateFiles(@"..\..\Data\Computers"))
-            {
-                string xmlString = XDocumentToStringConverter(file);
-                stringQueue.xmlConverterQueue.Enqueue(xmlString);
-            }
         }
     }
 }
